5cbeb15834c25658ec51550f28267a94e5e542ae
James Li <34412985+hakrrr@users.noreply.github.com>
2020-02-26 10:56:18 +0100

    Regex

--- Python/Challenge/Number6.py
+++ Python/Challenge/Number6.py
@@ -1,1 +1,11 @@
-# Hard
+# Regex & Overwrite Functions
+import re
+
+re.ASCII
+regex_integer_in_range = r"[1-9][0-9]{5}$"
+regex_alternating_repetitive_digit_pair = r"(?=(\d)\d\1)"
+re.findall = lambda ptt, s: len(list(re.finditer(ptt, s)))
+# regex_alternating_repetitive_digit_pair = r"(?P<numb>\d)\d(?P=numb)"
+
+print(bool(re.match(regex_integer_in_range, input())))
+print(re.findall(regex_alternating_repetitive_digit_pair, input()))
