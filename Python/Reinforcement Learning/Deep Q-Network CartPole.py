2eadba1da6a5589144c2ccca40c8f3577c7a4482
James Li <34412985+hakrrr@users.noreply.github.com>
2020-03-10 19:17:31 +0100

    Deep Q-Network

--- Python/Reinforcement Learning/Deep Q-Network CartPole.py
+++ Python/Reinforcement Learning/Deep Q-Network CartPole.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+
 # @https://deeplizard.com/learn/video/PyQNfsGUnQA
 
 
@@ -51,10 +52,105 @@ class ReplayMemory:
         return len(self.memory) >= batch_size
 
 
+class EpsilonGreedyStrategy:
+    def __init__(self, start, end, decay):
+        self.start = start
+        self.end = end
+        self.decay = decay
+
+    def get_exploration_rate(self, current_step):
+        return self.end + (self.start - self.end) * \
+            np.exp(-1. * self.decay * current_step)
+
+
+class Agent:
+    def __init__(self, strategy, num_actions, device):
+        self.current_step = 0
+        self.strategy = strategy
+        self.num_actions = num_actions
+        self.device = device
+
+    def select_action(self, state, policy_net):
+        rate = self.strategy.get_exploration_rate(self.current_step)
+        self.current_step += 1
+
+        if rate > random.random():
+            return random.randrange(self.num_actions)  # explore
+        else:
+            with torch.no_grad():
+                return policy_net(state).argmax(dim=1).to(self.device)  # exploit
+
+
+class CartPoleEnvManager:
+    def __init__(self, device):
+        self.device = device
+        self.env = gym.make('CartPole-v0').unwrapped
+        self.env.reset()
+        self.current_screen = None
+        self.done = False
+
+    def reset(self):
+        self.env.reset()
+        self.current_screen = None
+
+    def close(self):
+        self.env.close()
+
+    def render(self, mode='human'):
+        return self.env.render(mode)
+
+    def num_actions_available(self):
+        return self.env.action_space.n
+
+    def take_action(self, action):
+        _, reward, self.done, _ = self.env.step(action.item())
+        return torch.tensor([reward], device=self.device)
+
+    def just_starting(self):
+        return self.current_screen is None
+
+    def get_state(self):
+        if self.just_starting() or self.done:
+            self.current_screen = self.getprocessed_screen()
+            black_screen = torch.zeros_like(self.current_screen)
+            return black_screen
+        else:
+            s1 = self.current_screen
+            s2 = self.get_processed_screen()
+            self.current_screen = s2
+            return s2 - s1
+
+    def get_screen_height(self):
+        screen = self.get_processed_screen()
+        return screen.shape[2]
+
+    def get_screen_width(self):
+        screen = self.get_processed_screen()
+        return screen.shape[3]
+
+    def get_processed_screen(self):
+        screen = self.render('rgb_array').transpose((2, 0, 1))
+        screen = self.crop_screen(screen)
+        return self.transform_screen_data(screen)
+
+    def crop_screen(self, screen):
+        screen_height = screen.shape[1]
+        # Strip off top and bottom
+        top = int(screen_height * 0.4)
+        bottom = int(screen_height * 0.8)
+        screen = screen[:, top:bottom, :]
+        return screen
+
+    def transform_screen_data(self, screen):
+        # Convert to float, rescale, convert to tensor
+        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+        screen = torch.from_numpy(screen)
+
+        # Use torchvision package to compose image transforms
+        resize = T.Compose([T.ToPILImage(), T.Resize((40, 90)), T.ToTensor()])
+
+        return resize(screen).unsqeeze(0).to(self.device)  # add a batch dimension via unsqueeze
+
+
 Experience = namedtuple('Experience', ('state', 'action', 'next_state', 'reward'))
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    _, _, done, _ = env.step(env.action_space.sample())
-env.close()
+
