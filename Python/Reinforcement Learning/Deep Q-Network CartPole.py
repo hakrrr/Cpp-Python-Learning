4c1ee4aa9f09491dd6c076506b5e16b2110fb25b
James Li <34412985+hakrrr@users.noreply.github.com>
2020-03-11 17:30:28 +0100

    Deep Q-Network

--- Python/Reinforcement Learning/Deep Q-Network CartPole.py
+++ Python/Reinforcement Learning/Deep Q-Network CartPole.py
@@ -13,6 +13,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
+
 # @https://deeplizard.com/learn/video/PyQNfsGUnQA
 
 
@@ -20,7 +21,7 @@ class DQN(nn.Module):
     def __init__(self, img_height, img_width):
         super().__init__()
 
-        self.fc1 = nn.Linear(in_features=img_height*img_width*3, out_features=24)
+        self.fc1 = nn.Linear(in_features=img_height * img_width * 3, out_features=24)
         self.fc2 = nn.Linear(in_features=24, out_features=32)
         self.out = nn.Linear(in_features=32, out_features=2)
 
@@ -60,7 +61,7 @@ class EpsilonGreedyStrategy:
 
     def get_exploration_rate(self, current_step):
         return self.end + (self.start - self.end) * \
-            np.exp(-1. * self.decay * current_step)
+               np.exp(-1. * self.decay * current_step)
 
 
 class Agent:
@@ -75,7 +76,8 @@ class Agent:
         self.current_step += 1
 
         if rate > random.random():
-            return random.randrange(self.num_actions)  # explore
+            action = random.randrange(self.num_actions)
+            return torch.tensor([action]).to(self.device)  # explore
         else:
             with torch.no_grad():
                 return policy_net(state).argmax(dim=1).to(self.device)  # exploit
@@ -111,7 +113,7 @@ class CartPoleEnvManager:
 
     def get_state(self):
         if self.just_starting() or self.done:
-            self.current_screen = self.getprocessed_screen()
+            self.current_screen = self.get_processed_screen()
             black_screen = torch.zeros_like(self.current_screen)
             return black_screen
         else:
@@ -150,7 +152,26 @@ class CartPoleEnvManager:
         # Use torchvision package to compose image transforms
         resize = T.Compose([T.ToPILImage(), T.Resize((40, 90)), T.ToTensor()])
 
-        return resize(screen).unsqeeze(0).to(self.device)  # add a batch dimension via unsqueeze
+        return resize(screen).unsqueeze(0).to(self.device)  # add a batch dimension via unsqueeze
+
+
+class QValues:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    @staticmethod
+    def get_current(policy_net, states, actions):
+        return policy_net(states).gather(dim=1, index=actions.unsqueeze(-1))
+
+    @staticmethod
+    def get_next(target_net, nextstates):
+        final_state_locations = next_states.flatten(start_dim=1) \
+            .max(dim=1)[0].eq(0).type(torch.bool)
+        non_final_state_locations = (final_state_locations == False)
+        non_final_states = next_states[non_final_state_locations]
+        batch_size = next_states.shape[0]
+        values = torch.zeros(batch_size).to(QValues.device)
+        values[non_final_state_locations] = target_net(non_final_states).max(dim=1)[0].detach()
+        return values
 
 
 def plot(values, moving_avg_period):
@@ -160,8 +181,12 @@ def plot(values, moving_avg_period):
     plt.xlabel('Episode')
     plt.ylabel('Duration')
     plt.plot(values)
+
+    moving_avg = get_moving_average(moving_avg_period, values)
     plt.plot(get_moving_average(moving_avg_period, values))
     plt.pause(0.001)
+    print('Episode', len(values), '\n',
+          moving_avg_period, 'episode moving avg: ', moving_avg[-1])
 
 
 def get_moving_average(period, values):
@@ -169,11 +194,22 @@ def get_moving_average(period, values):
     if len(values) >= period:
         moving_avg = values.unfold(dimension=0, size=period, step=1) \
             .mean(dim=1).flatten(start_dim=0)
-        moving_avg = torch.cat((torch.zeros(period-1), moving_avg))
+        moving_avg = torch.cat((torch.zeros(period - 1), moving_avg))
         return moving_avg.numpy()
     else:
         moving_avg = torch.zeros(len(values))
         return moving_avg.numpy()
+
+
+def extract_tensors(experiences):
+    batch = Experience(*zip(*experiences))
+
+    t1 = torch.cat(batch.state)
+    t2 = torch.cat(batch.action)
+    t3 = torch.cat(batch.reward)
+    t4 = torch.cat(batch.next_state)
+
+    return t1, t2, t3, t4
 
 
 # main()
@@ -201,3 +237,38 @@ target_net.eval()
 optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
 
 # Training Loop
+episode_durations = []
+for episode in range(num_episodes):
+    em.reset()
+    state = em.get_state()
+
+    for timestep in count():
+        action = agent.select_action(state, policy_net)
+        reward = em.take_action(action)
+        next_state = em.get_state()
+        memory.push(Experience(state, action, next_state, reward))
+        state = next_state
+
+        if memory.can_provide_sample(batch_size):
+            experiences = memory.sample(batch_size)
+            states, actions, rewards, next_states = extract_tensors(experiences)
+
+            current_q_values = QValues.get_current(policy_net, states, actions)
+            next_q_values = QValues.get_next(target_net, next_states)
+            target_q_values = (next_q_values * gamma) + rewards
+
+            # Gradient descent
+            loss = F.mse_loss(current_q_values, target_q_values.unsqueeze(1))
+            optimizer.zero_grad()  # Reset Gradients
+            loss.backward()  # Compute Gradients
+            optimizer.step()  # Update NN
+
+        if em.done:
+            episode_durations.append(timestep)
+            plot(episode_durations, 100)
+            break
+
+    if episode % target_update == 0:
+        target_net.load_state_dict((policy_net.state_dict()))
+
+em.close()
